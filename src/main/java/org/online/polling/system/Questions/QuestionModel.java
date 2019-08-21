cc58fee75ffaf748096b7f37a909341457286e00
shubhamshettyy <shubham.shetty@spit.ac.in>
2019-08-22 00:13:47 +0530

    Commiting changes

--- src/main/java/org/online/polling/system/Questions/QuestionModel.java
+++ src/main/java/org/online/polling/system/Questions/QuestionModel.java
@@ -1,4 +1,17 @@
 package org.online.polling.system.Questions;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/questions")
 public class QuestionModel {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getQuestions(){
+        return "Questions is up";
+    }
+
 }
