03a1c6f7f52289e6d8420d3b75060018fb7f8e77
shubhamshettyy <shubham.shetty@spit.ac.in>
2019-08-22 23:16:14 +0530

    Added Add Message

--- src/main/java/org/online/polling/system/Questions/QuestionResource.java
+++ src/main/java/org/online/polling/system/Questions/QuestionResource.java
@@ -5,32 +5,40 @@ import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 @Path("/questions")
-@Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class QuestionResource {
     QuestionService questionService = new QuestionService();
 
     @GET
-    public List<QuestionModel> getQuestions()
-    {
+    public List<QuestionModel> getQuestions() {
         return questionService.getAllQuestions();
     }
 
     @PUT
     @Path("/{question_id}")
-    public QuestionModel updateQuestion(@PathParam("question_id") long id,QuestionModel question)
-    {
+    public QuestionModel updateQuestion(@PathParam("question_id") long id, QuestionModel question) {
         question.setId(id);
         return questionService.updateQuestion(question);
     }
 
-    //@POST
 
+    @POST
+    public QuestionModel addQuestion(QuestionModel question)
+    {
+        return questionService.addQuestion(question);
+    }
+
+
+    @GET
+    @Path("/{question_id}")
+    public QuestionModel getQuestion(@PathParam("question_id") long id) {
+        return questionService.getQuestion(id);
+    }
 
     @DELETE
     @Path("{question_id}")
-    public void deleteQuestion(@PathParam("question_id") long id)
-    {
+    public void deleteQuestion(@PathParam("question_id") long id) {
 //        question.setId(id);
         questionService.removeQuestion(id);
     }
--- src/main/java/org/online/polling/system/Questions/QuestionService.java
+++ src/main/java/org/online/polling/system/Questions/QuestionService.java
@@ -11,13 +11,19 @@ public class QuestionService {
     private Map<Long,QuestionModel> questionsMap= DatabaseClass.getQuestions();
 
     public QuestionService() {
-        questionsMap.put(1L,new QuestionModel(1L,"Q11111","A","B","C","D"));
+        questionsMap.put(1L,new QuestionModel(1L,"Q1","A","B","C","D"));
         questionsMap.put(2L,new QuestionModel(2L,"Q2","A","B","C","D"));
     }
 
     public List<QuestionModel> getAllQuestions()
     {
+
         return new ArrayList<QuestionModel>(questionsMap.values());
+    }
+
+    public QuestionModel getQuestion(long id)
+    {
+        return questionsMap.get(id);
     }
 
     public QuestionModel addQuestion(QuestionModel question)
@@ -33,13 +39,13 @@ public class QuestionService {
 //        {
 //            return null;
 //        }
-//        System.out.println(question);
         questionsMap.put(question.getId(),question);
         return question;
     }
 
     public QuestionModel removeQuestion(long id)
     {
+
         return questionsMap.remove(id);
     }
 }
